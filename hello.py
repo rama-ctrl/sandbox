7a05d5baf306382645d98263109f5da7f5512bfb
rama-ctrl <hectoradventures@gmail.com>
2021-05-19 05:40:30 +0530

    C1

---
+++ hello.py
@@ -0,0 +1,18 @@
+#1. Table of number n
+"""
+n = int(input("Enter the number "))  
+for i in range(1,11):  
+    c = n*i  
+    print n,"*",i,"=",c
+
+"""
+
+
+#2. Print Even number btween 2 and n
+"""
+n = int(input("Enter the number "))  
+for i in range(2,n,2):  
+    print(i)  
+
+"""
+
