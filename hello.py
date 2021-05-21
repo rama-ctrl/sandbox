0f38a51688cff59088a90abf4770a4b3e1cf6974
rama-ctrl <hectoradventures@gmail.com>
2021-05-21 15:08:12 +0530

    c1

--- hello.py
+++ hello.py
@@ -73,7 +73,7 @@ for i in range(0,n+1):
 """
 
 #6. Cuberoot through bisection algorithm/aproaximation
-
+"""
 n = float(input("Enter number to find cuberoot of: "))
 epsilon = 0.001
 counter = 0
@@ -96,7 +96,32 @@ if abs(n) >1:
     print("number of iteration:",counter)
     print("Cuberoot of ",n,"is",guess)
 
+if abs(n)<1:
+    low = n
+    high = 1
+    guess = (high+low)/2.0
 
+    while abs (abs(guess)**3 - abs(n)) >= epsilon:
+        if n > 0:
+            if guess**3 < n:
+                low = guess
+            else:
+                high = guess
+
+            guess = (high+low)/2.0
+            counter += 1
+        else:
+            low = -1
+            high = n
+            if guess ** 3 > n:
+                high = guess
+            else:
+                low = guess
+            guess = (low + high) / 2.0
+            counter+= 1
+    print(counter)
+    print("The cube root of",n,"is closest to:",guess)
+"""
 
 
 #7. String manipulation
@@ -108,3 +133,54 @@ for char in string1:
     i+=1
 
 """
+
+#8. lambda function
+"""
+lst = (10,22,37,41,100,123,29)  
+oddlist = tuple(filter(lambda x:(x%2 != 0),lst)) # the tuple contains all the items of the tuple for which the lambda function evaluates to true    
+print(oddlist)  
+
+"""
+
+
+
+#9 file handling
+"""
+from os import write
+import os
+
+fileObject = open("hello.txt","w")
+fileObject.write("This message is appended from terminal.")
+fileObject.close()
+
+
+print("Changing file again")
+
+change2 = open("hello.txt","a")
+change2.write(" This is second update in file and it should be appended in last of the files, not ovewrite")
+change2.close()
+
+file = open("hello.txt","r")
+content = file.read(2000)
+
+print(type(content))
+#print(content)
+
+for i in file:
+    print(i)
+
+
+file.close()
+
+os.getcwd()
+
+"""
+
+
+
+
+
+#10. Modules
+
+import testModule
+
